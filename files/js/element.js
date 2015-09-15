ad63721196b58a1f53a4c791f383cb3782973bfb
puddi <puddizzle@gmail.com>
2015-09-15 16:55:50 -0700

    Deleting old comments from old versions

--- files/js/element.js
+++ files/js/element.js
@@ -1,8 +1,6 @@
 /**
  * This is required for element rendering to be possible
  * @type {PlatformElement}
- *
- * handles the logic of determining which table template to keep around, as well as swapping cells in and out from old tables to new ones.
  */
 (function() {
 	var SimpleTable = PlatformElement.extend({
