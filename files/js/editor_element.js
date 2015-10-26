a023326dce6b8bd88ed0fcdea1aa324e8c1ce586
puddi <puddizzle@gmail.com>
2015-10-26 15:26:09 -0700

    better functionality for keeping table column sizes on adding/deleting new columns.

--- files/js/editor_element.js
+++ files/js/editor_element.js
@@ -4,6 +4,9 @@
  */
 (function() {
     var SimpleTable = PlatformElement.extend({
+        // default/minimum column width, in a percentage-based format
+        minColumnWidth: 5,
+
         // a lot of setup is here; setting the sizes of the columns,
         // normalizing styles, and making the table resizable.
         initialize: function() {
@@ -33,7 +36,7 @@
         listenForPlaceholderReplacement: function() {
             var view = this;
             this.placeholderTimeout = setInterval(function() {
-                if (this.$('.platform-element-child-placeholder').length == 0) {
+                if (this.$('.platform-element-child-placeholder').length === 0) {
                     this.fixStyles();
                     this.setUpResizable();
                     clearInterval(this.placeholderTimeout);
@@ -45,15 +48,63 @@
         setSizes: function() {
             var sizes = this.settings.get('tableSizes');
             if (sizes && sizes != "default") {
-                // if the # of columns isn't the size of the array of lengths, ignore it.
-                if (this.settings.get('columns') == sizes.length) {
-                    var columns = this.$('tr:first-child td').map(function(index, value) {
-                        $(value).css('width', sizes[index] + '%');
+                if (this.settings.get('columns') > sizes.length) {
+                    // we need to make more columns.
+                    // to do this, we take each existing column and proportionally remove a bit of width to make room.
+                    // no columns will ever go under the minimum column size.
+                    // new columns will have the minimum column size.
+
+                    // get normalized total width
+                    // i.e., [45, 5, 50] => [40, 0, 45] => 95
+                    var normalizedTotalWidth = sizes.reduce(function(a, b) { return a + b; }, 0) - sizes.length * this.minColumnWidth;
+
+                    // the number of columns we have to add
+                    var diff = this.settings.get('columns') - sizes.length;
+
+                    // then subtract porportionally to make room for the new columns
+                    // i.e. [45, 5, 50] => [42.74, 5, 47.26]
+                    // (more was taken out of 50 than of 45, and the total sum is now 95 instead of 100)
+                    sizes.map(function(a) {
+                        return a - ((a - this.minColumnWidth) * ((diff * this.minColumnWidth) / normalizedTotalWidth));
+                    }.bind(this));
+
+                    // then add new columns.
+                    for (var i = 0; i < diff; i++) {
+                        sizes.push(this.minColumnWidth);
+                    }
+
+                    // save the new array of widths.
+                    this.settings.set('tableSizes', sizes);
+                    this.settings.save();
+                } else if (this.settings.get('columns') < sizes.length) {
+                    // we need to remove the extra columns.
+                    // proportionally redistribute the columns that will get deleted;
+                    // i.e., [40, 20, 20, 20] would become [50, 25, 25] if the last column got deleted
+
+                    // get total size of elements being removed
+                    var size = 0;
+                    while (this.settings.get('columns') != sizes.length) {
+                        size += sizes.pop();
+                    }
+
+                    // get total sizes of remaining widths
+                    var totalWidth = sizes.reduce(function(a, b) { return a + b; }, 0);
+
+                    // then redistribute sizes accordingly
+                    sizes.map(function(a) {
+                        a += size * (a / totalWidth);
                     });
-                } else {
-                    this.settings.unset('tableSizes');
+
+                    // save the new array of widths
+                    this.settings.set('tableSizes', sizes);
                     this.settings.save();
                 }
+
+                // now sizes is an array w/ the proper number of columns
+                // apply the sizes!
+                var columns = this.$('tr:first-child td').map(function(index, value) {
+                    $(value).css('width', sizes[index] + '%');
+                });
             }
         },
 
